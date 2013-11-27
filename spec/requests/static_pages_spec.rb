5e2d707056dd9e35d2a560657756cbe1c441cdb8
Gabe Lafontant <glafonta@gmail.com>
2013-11-27 13:12:51 -0500

    new test for contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,16 @@ describe "StaticPages" do
       expect(page).to have_title("About Us") 
     end
   end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit "/static_pages/contact"
+      expect(page).to have_title("Contact Us")
+    end
+  end
 end
