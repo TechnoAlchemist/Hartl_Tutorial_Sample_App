371e47308dd12028dd025cfcf1fe74710bba1e27
Gabe Lafontant <glafonta@gmail.com>
2013-12-07 16:21:31 -0500

    changes to contact view

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,26 +32,26 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
+  	it "should have the content 'About'" do
   		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
+  		expect(page).to have_content('About')
   	end
 
-    it "should have the title 'About Us'" do
+    it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("About Us") 
+      expect(page).to have_title("About") 
     end
   end
 
   describe "Contact Page" do
-    it "should have the content 'Contact Us'" do
+    it "should have the content 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
+      expect(page).to have_content('Contact')
     end
 
-    it "should have the title 'Contact Us'" do
+    it "should have the title 'Contact'" do
       visit "/static_pages/contact"
-      expect(page).to have_title("Contact Us")
+      expect(page).to have_title("Contact")
     end
   end
 end
