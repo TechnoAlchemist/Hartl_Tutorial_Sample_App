77fdf60ed01037f973fd0ca8201531147712af29
Gabe Lafontant <glafonta@gmail.com>
2013-12-13 16:28:30 -0500

    validations for email using regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates_presence_of :name
-	validates_presence_of :email
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates_format_of :email, with: VALID_EMAIL_REGEX
+	validates_length_of :name, maximum: 50
 end
