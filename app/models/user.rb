6ff76126bff4a57ba5fe6cd80beba64f55e550c4
Gabe Lafontant <glafonta@gmail.com>
2013-12-14 19:55:14 -0500

    passing test suite for authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
 	validates_format_of     :email, with: VALID_EMAIL_REGEX
 	validates_uniqueness_of :email, case_sensitive: false 
 
-	#has_secure_password
+	validates_length_of :password, minimum: 6
+	has_secure_password
 	
 
 end
