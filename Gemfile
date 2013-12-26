746b9fbc99492c8dd8df1e2b9e2b10b98f34053a
Gabe Lafontant <glafonta@gmail.com>
2013-12-26 18:33:24 -0500

    test for error flash message

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.7'
   gem 'rspec-rails', '2.13.1'
   gem 'pry-rails'
+  gem 'launchy'
 end
 
 group :test do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# Sign th euser in and redirect to the user's show page.
+		else
+			# Create an error message and re-render the signin form.
+			flash[:error] = "Invalid email/password combination"
+			render 'new'
+		end
 	end
 
 	def destroy
