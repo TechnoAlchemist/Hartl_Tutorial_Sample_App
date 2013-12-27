edd31b82e9c46912c95bc64cb8d2bd6db6ed3f5e
Gabe Lafontant <glafonta@gmail.com>
2013-12-27 15:10:47 -0500

    user model with callback for remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,28 @@
 class User < ActiveRecord::Base
 	before_save { email.downcase! }
-	validates_presence_of :name
-	validates_length_of   :name, maximum: 50
-	
+	before_create :create_remember_token
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-						
+	
+	validates_presence_of 	:name
+	validates_length_of   	:name, maximum: 50
 	validates_format_of     :email, with: VALID_EMAIL_REGEX
 	validates_uniqueness_of :email, case_sensitive: false 
-
-	validates_length_of :password, minimum: 6
-	has_secure_password
+	validates_length_of 	:password, minimum: 6
 	
+	has_secure_password
 
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
 end
